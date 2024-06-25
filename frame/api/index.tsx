ef69eedb72402c538b69be117af55b8168db8db7
aakash-taneja <aakashtaneja38@gmail.com>
2024-06-26 00:21:51 +0530

    update

--- frame/api/index.tsx
+++ frame/api/index.tsx
@@ -25,9 +25,92 @@ export const app = new Frog({
   // },
 });
 
-app.frame("/", (c) => {
-  const { status } = c;
-  // const cid = c.req.param("cid");
+app.transaction("/mint", (c) => {
+  const address = c.address;
+  console.log(`${address}`);
+  let tx;
+
+  tx = c.contract({
+    abi,
+    chainId: "eip155:84532",
+    functionName: "mintNFT",
+    args: [
+      1,
+      //nftId
+      "0xbDA5747bFD65F08deb54cb465eB87D40e51B197E", // affiliate (address)
+      1, // amount
+    ],
+    to: "0x2a12e7bcC4662227Ddd28b60627d9dc25598be1A",
+    // value: amountInWei,
+  });
+
+  return tx;
+});
+app.transaction("/approve", (c) => {
+  const address = c.address;
+  // Contract transaction response.
+  console.log(`${address}`);
+  let tx;
+
+  tx = c.contract({
+    abi: abidegen,
+    chainId: "eip155:84532",
+    functionName: "approve",
+    args: ["0x2a12e7bcC4662227Ddd28b60627d9dc25598be1A", parseEther("1000")],
+    to: "0x76160579627CD45Ba88f23E5919C61444AF53D5A",
+    // value: amountInWei,
+  });
+
+  return tx;
+});
+
+app.frame("/:cid", async (c) => {
+  const cid = c.req.param("cid");
+  const data = await fetch(`https://gateway.lighthouse.storage/ipfs/${cid}`);
+  const res = JSON.parse(await data.text());
+
+  return c.res({
+    action: `/second/${cid}`,
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background: "linear-gradient(to right, #432889, #17101F)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {res.text}
+        </div>
+      </div>
+    ),
+
+    intents: [<Button>View Product</Button>],
+  });
+});
+
+app.frame("/second/:cid", async (c) => {
+  const cid = c.req.param("cid");
+  const data = await fetch(`https://gateway.lighthouse.storage/ipfs/${cid}`);
+  const res = JSON.parse(await data.text());
   return c.res({
     image: (
       <div
@@ -56,79 +139,33 @@ app.frame("/", (c) => {
             whiteSpace: "pre-wrap",
           }}
         >
-          {/* {status === "response" ? `Done` : `passed cid is : ${cid}`} */}
+          {`Title: ${res.product.productTitle}`}
         </div>
-        {/* <img
-          height={"150px"}
-          width={"200px "}
-          src={`https://gateway.lighthouse.storage/ipfs/${cid}`}
-        /> */}
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {`Price: ${res.product.productPrice}`}
+        </div>
       </div>
     ),
+
     intents: [
+      <Button.Link href="https://www.google.com">Buy now</Button.Link>,
       <Button.Transaction target="/approve">Approve</Button.Transaction>,
-      <Button.Transaction target="/mint">Collect</Button.Transaction>,
-      <Button.Transaction target="/addNFT">Add NFT</Button.Transaction>,
-      status === "response" && <Button.Reset>Reset</Button.Reset>,
+      <Button.Transaction target="/mint">Mint now</Button.Transaction>,
     ],
   });
 });
 
-app.transaction("/approve", (c) => {
-  const address = c.address;
-  // Contract transaction response.
-  console.log(`${address}`);
-  let tx;
-
-  tx = c.contract({
-    abi: abidegen,
-    chainId: "eip155:84532",
-    functionName: "approve",
-    args: ["0x701582a0b6ee50EB06a463bADB7545c5ac5adbBE", parseEther("1000")],
-    to: "0x22A4FA38D3E0a3E791FDb0b9C48Fd42FBCFeE84b",
-    // value: amountInWei,
-  });
-
-  return tx;
-});
-app.transaction("/addNFT", (c) => {
-  const address = c.address;
-  // Contract transaction response.
-  console.log(`${address}`);
-  let tx;
-
-  tx = c.contract({
-    abi,
-    chainId: "eip155:84532",
-    functionName: "addNFT",
-    args: [`${Date.now()}`, "100000000000000000", "1000"],
-    to: "0x701582a0b6ee50EB06a463bADB7545c5ac5adbBE",
-    // value: amountInWei,
-  });
-
-  return tx;
-});
-app.transaction("/mint", (c) => {
-  const address = c.address;
-  console.log(`${address}`);
-  let tx;
-
-  tx = c.contract({
-    abi,
-    chainId: "eip155:84532",
-    functionName: "mintNFT",
-    args: [
-      1,
-      //nftId
-      "0xbDA5747bFD65F08deb54cb465eB87D40e51B197E", // affiliate (address)
-      1, // amount
-    ],
-    to: "0x701582a0b6ee50EB06a463bADB7545c5ac5adbBE",
-    // value: amountInWei,
-  });
-
-  return tx;
-});
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== "undefined";
 const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
